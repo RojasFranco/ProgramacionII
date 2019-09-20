dcc3a58ac226546f201cfbb38ae3ba9d62c416df
alumno <alumno@utnfra.com.ar>
2019-09-19 22:10:03 -0300

    Ejercicio 37

---
+++ Clase9-Herencia/CentralTelefonica/CentralitaHerencia/Llamada.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralitaHerencia
+{
+    public class Llamada
+    {
+        protected float duracion;
+        protected string nroDestino;
+        protected string nroOrigen;
+
+
+        #region Propiedades
+
+        public float Duracion
+        {
+            get
+            {
+                return this.duracion;
+            }
+        }
+
+        public string NroDestino
+        {
+            get
+            {
+                return this.nroDestino;
+            }
+        }
+
+        public string NroOrigen
+        {
+            get
+            {
+                return this.nroOrigen;
+            }
+        }
+
+        #endregion
+
+
+        #region Constructor
+
+        public Llamada(float duracion, string nroDestino, string nroOrigen)
+        {
+            this.duracion = duracion;
+            this.nroDestino = nroDestino;
+            this.nroOrigen = nroOrigen;            
+        }
+
+        #endregion
+
+
+        #region Metodos
+
+        public string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat(" Duracion : {0}", this.Duracion);
+            sb.AppendFormat(" Nro destino: {0} ", this.NroDestino);
+            sb.AppendFormat(" Nro origen: {0}", this.NroOrigen);
+            return sb.ToString();            
+
+        }
+
+        public int OrdenarPorDuracion(Llamada llamada1, Llamada llamada2)
+        {
+            if(llamada1.Duracion<llamada2.Duracion)
+            {
+                return -1;
+            }
+            else if(llamada1.Duracion == llamada2.Duracion)
+            {
+                return 0;
+            }
+            return 1;
+        }
+
+        #endregion
+
+        public enum TipoLlamada
+        {
+            Local,
+            Provincial,
+            Todas
+        }
+    }
+}
---
+++ Clase9-Herencia/CentralTelefonica/CentralitaHerencia/Provincial.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralitaHerencia
+{
+    public class Provincial : Llamada
+    {
+
+        protected Franja franjaHoraria;
+
+
+        #region Propiedades
+
+        public float CostoLlamada
+        {
+            get
+            {
+                return CalcularCosto();
+            }
+        }
+
+        #endregion
+
+
+        #region Constructores
+
+        public Provincial(Franja miFranja, Llamada llamada) : base(llamada.Duracion, llamada.NroDestino, llamada.NroOrigen)
+        {
+            this.franjaHoraria = miFranja;
+        }
+
+        public Provincial(string origen, Franja miFranja, float duracion, string destino) : base(duracion, destino, origen)
+        {
+            this.franjaHoraria = miFranja;
+        }
+
+        #endregion
+
+
+        #region Metodos
+
+        private float CalcularCosto()
+        {
+            if(this.franjaHoraria == Franja.Franja_1)
+            {
+                return this.Duracion * 0.99f;
+            }
+            else if(this.franjaHoraria == Franja.Franja_2)
+            {
+                return this.Duracion * 1.25f;
+            }
+            return this.Duracion * 0.66f;
+
+        }
+
+        public string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(base.Mostrar());
+            sb.AppendFormat(" Costo llamada : {0} ", this.CostoLlamada);
+            sb.AppendFormat(" Franja horaria: {0} ", this.franjaHoraria);
+            return sb.ToString();
+        }
+
+        #endregion
+
+
+        public enum Franja
+        {
+            Franja_1,
+            Franja_2,
+            Franja_3
+        }
+    }
+}
